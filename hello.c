43399a14086adb7add4a011323dfbbb4cbb3653f
jklusaw3 <jklusaw3@huskers.unl.edu>
2021-08-24 21:36:31 -0500

    Initial commit
    Additional text to state a major.

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+* Author: Jason Klusaw
+*
+* Date: 2021/8/24
+*
+* Simple text script.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void main() 
+{
+    printf("Jason Klusaw"\n);
+    printf("Undeclared"\n):
+}
